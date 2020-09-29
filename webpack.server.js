84f41e3d62afd4931891d9c249e8742207240370
Hector <hochoa@gmail.com>
2020-09-29 16:58:39 -0500

    Hero Updates

--- webpack.server.js
+++ webpack.server.js
@@ -4,7 +4,7 @@ const dev = require("./webpack.dev.js")
 
 module.exports = merge(dev, {
   devServer: {
-    host: "https://fall.lightnfluffy.test/",
+    host: "https://fall.lightnfluffy.com/",
     port: 8080,
     headers: {
       "Access-Control-Allow-Origin": "*"
